c363f2bf0789ca8cc4589fa396b64760d596e297
ayushh-gautam <ayushgautam896@gmail.com>
2023-02-03 22:51:47 +0545

    did some changes

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -2,8 +2,15 @@
 import 'package:flutter/material.dart';
 import 'widgets.dart';
 
+const activeBoxColors = Color(0xFF1D1E33);
+const inactiveBoxColors = Color(0xFF101427);
 const containerHeight = 80.0;
-const boxColors = 0xFF1D1E33;
+const containerColor = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+}
 
 class BmiApp extends StatefulWidget {
   const BmiApp({super.key});
@@ -13,6 +20,8 @@ class BmiApp extends StatefulWidget {
 }
 
 class _BmiAppState extends State<BmiApp> {
+  Gender? selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,29 +33,61 @@ class _BmiAppState extends State<BmiApp> {
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: CustomCard(colour: Color(boxColors))),
                   Expanded(
-                    child: CustomCard(colour: Color(boxColors)),
+                      child: CustomCard(
+                    onPressed: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    colour: selectedGender == Gender.male
+                        ? activeBoxColors
+                        : inactiveBoxColors,
+                    cardChild: IconWidget(
+                      icon: (Icons.male),
+                      label: 'MALE',
+                    ),
+                  )),
+                  Expanded(
+                    child: CustomCard(
+                      onPressed: () {
+                        setState(() {
+                          selectedGender = Gender.female;
+                        });
+                      },
+                      colour: selectedGender == Gender.female
+                          ? activeBoxColors
+                          : inactiveBoxColors,
+                      cardChild: IconWidget(
+                        icon: (Icons.female),
+                        label: 'FEMALE',
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            Expanded(child: CustomCard(colour: Color(boxColors))),
+            Expanded(child: CustomCard(colour: activeBoxColors)),
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: CustomCard(colour: Color(boxColors))),
-                  Expanded(child: CustomCard(colour: Color(boxColors)))
+                  Expanded(child: CustomCard(colour: activeBoxColors)),
+                  Expanded(child: CustomCard(colour: activeBoxColors))
                 ],
               ),
             ),
             Container(
-              margin: EdgeInsets.only(top: 10.0),
+              margin: EdgeInsets.only(top: 9.0),
+              decoration: BoxDecoration(
+                  color: containerColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(9),
+                      topRight: Radius.circular(9))),
               height: containerHeight,
               width: double.infinity,
-              color: Colors.pink.shade700,
             )
           ],
         ));
   }
 }
+//14:44
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -2,17 +2,49 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class CustomCard extends StatelessWidget {
-  CustomCard({super.key, @required this.colour});
+  CustomCard({super.key, @required this.colour, this.cardChild, this.onPressed});
+  final Widget? cardChild;
   Color? colour;
+  final Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(9),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        child: cardChild,
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(9),
+        ),
       ),
+    );
+  }
+}
+
+class IconWidget extends StatelessWidget {
+  final IconData? icon;
+  final String? label;
+  IconWidget({this.icon, this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          label!,
+          style: TextStyle(color: Color(0xFF808E98)),
+        )
+      ],
     );
   }
 }
